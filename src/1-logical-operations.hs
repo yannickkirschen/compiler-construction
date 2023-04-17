38e3c750c8bd376205f39708567893a72f2dbf0f
Yannick Kirschen <yannickkirschen@protonmail.com>
2023-04-17 10:04:21 +0200

    chore: reformat using haskell code style

--- src/1-logical-operations.hs
+++ src/1-logical-operations.hs
@@ -5,27 +5,31 @@
 -- and a full-adder for binary numbers.
 
 and_ :: (Bool, Bool) -> Bool
-and_ (a, b) | a == True && b == True = True
-            | otherwise = False
+and_ (a, b)
+  | a && b = True
+  | otherwise = False
 
 or_ :: (Bool, Bool) -> Bool
-or_ (a, b) | a == True || b == True = True
-           | otherwise = False
+or_ (a, b)
+  | a || b = True
+  | otherwise = False
 
 xor_ :: (Bool, Bool) -> Bool
-xor_ (a, b) | a == True && b == False = True
-            | a == False && b == True = True
-            | otherwise = False
+xor_ (a, b)
+  | a && b = True
+  | not a && b = True
+  | otherwise = False
 
 neg :: Bool -> Bool
-neg a | a == True = False
-      | otherwise = True
+neg a
+  | a = False
+  | otherwise = True
 
-half_adder :: (Bool, Bool) -> (Bool, Bool)
-half_adder (a, b) = (xor_(a, b), and_(a, b))
+halfAdder :: (Bool, Bool) -> (Bool, Bool)
+halfAdder (a, b) = (xor_ (a, b), and_ (a, b))
 
-full_adder :: (Bool, Bool, Bool) -> (Bool, Bool)
-full_adder (a, b, c) = let
-                            (e1, c1) = half_adder(a, b)
-                            (e2, c2) = half_adder(c, e1)
-                       in (e2, or_(c1, c2))
+fullAdder :: (Bool, Bool, Bool) -> (Bool, Bool)
+fullAdder (a, b, c) =
+  let (e1, c1) = halfAdder (a, b)
+      (e2, c2) = halfAdder (c, e1)
+   in (e2, or_ (c1, c2))
--- src/1-symbolic-differentiation.hs
+++ src/1-symbolic-differentiation.hs
@@ -5,14 +5,15 @@
 -- the derivative of that polynomial. The polynomial should be represented as a
 -- Haskell data type. The derivative of a polynomial is another polynomial.
 
-data Term = Mon (Float, Int)
-          | Add (Term, Term)
-          | Mul (Term, Term)
-          | Div (Term, Term)
-            deriving (Show)
+data Term
+  = Mon (Float, Int)
+  | Add (Term, Term)
+  | Mul (Term, Term)
+  | Div (Term, Term)
+  deriving (Show)
 
 diff :: Term -> Term
-diff (Mon (a, b)) = Mon (a * fromIntegral (b), b - 1)
+diff (Mon (a, b)) = Mon (a * fromIntegral b, b - 1)
 diff (Add (a, b)) = Add (diff a, diff b)
 diff (Mul (a, b)) = Add (Mul (diff a, b), Mul (a, diff b))
 diff (Div (a, b)) = Div (Add (Mul (diff a, b), Mul (Mul (a, Mon (-1, 0)), diff b)), Mul (b, b))
